dbda6311808a01a15df34e2f4ad88b3fa5f4d368
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-10-05 20:35:03 +0200

    This is where it starts

---
+++ examples/api/base-model.R
@@ -0,0 +1,22 @@
+validators <- list(
+  b = function(x) ifelse(x == 0, 1, x)
+)
+
+my_model <- base_model(
+  a = is.numeric,
+  b = is.numeric,
+  .validators = validators
+)
+
+f <- function(a, b) a / b
+
+do.call(f, my_model(a = 1, b = 0)) # 1
+do.call(f, list(a = 1, b = 0)) # Inf
+
+# Use model inside function
+f <- function(a, b) {
+  params = my_model(environment())
+  params$a / params$b
+}
+
+f(1, 0) # 1
---
+++ examples/base_model.R
@@ -0,0 +1,35 @@
+my_model <- base_model(
+  a = is.numeric,
+  b = is.numeric,
+  txt = is.character,
+  .validators = list(
+    b = function(x) as.numeric(x) * 20
+  )
+)
+
+x <- list(
+  a = 10,
+  b = "20",
+  txt = "yes"
+)
+
+my_model(x)
+
+f <- function(a, b, txt) {
+  args <- my_model(environment())
+  return(args)
+}
+
+f(10, "20", "yes")
+
+# ---
+another_model <- base_model(
+  a = function(x) is.null(x) | is.numeric(x),
+  b = is.numeric,
+  txt = is.character
+)
+
+another_model(
+  list(a = NULL, b = 10, txt = "Hello"),
+  .drop_null = TRUE
+)
