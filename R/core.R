af2e833eddf7009e1d9625ac97cbd4c6e0923ce5
Nico Frieß <nico.friess@posteo.de>
2024-10-06 22:28:26 +0200

    suggestion for model derivation

--- R/core.R
+++ R/core.R
@@ -1,10 +1,12 @@
 check_types <- function(types, validators = NULL) {
+
   function(.obj = list(), ..., .drop_null = TRUE) {
     if (is.environment(.obj)) .obj <- as.list(.obj)
 
     .obj <- utils::modifyList(.obj, list(...))
 
     if (!is.null(validators)) {
+
       for (k in names(validators)) {
         .obj[[k]] <- validators[[k]](.obj[[k]])
       }
@@ -31,6 +33,42 @@ check_types <- function(types, validators = NULL) {
 #' @example examples/api/base-model.R
 #' @export
 base_model <- function(..., .validators = NULL) {
-  types <- list(...)
+  types <- unlist(list(...))
   return(check_types(types, .validators))
 }
+
+
+template <- list(a_character = "hello",
+                 a_model = lm(mpg ~ cyl, data = mtcars),
+                 a_dataframe = mtcars,
+                 a_number = 12,
+                 an_integer = 12L)
+
+
+derive_model <- function(template){
+  if(!is.list(template)){
+    stop ("Please provide a list of key/value pairs with the value used for type derivation.)")
+  }
+  validators <- lapply(template, function(value){
+    type <- typeof(value)
+    class <- class(value)
+    mode <- mode(value)
+    storage.mode <- storage.mode(value)
+    validator <- function(x){
+      (typeof(x) == eval(type) &&
+         class(x) == class &&
+         mode(x) == mode)
+    }
+    return(validator)
+  })
+  base_model(setNames(validators,names(template)))
+
+}
+
+my_model <- derive_model(template)
+my_model(list(
+  an_integer = 12L,
+  a_character = "test",
+  a_model = lm(Sepal.Width ~ Petal.Width, data = iris),
+  a_number = 7
+))
